56e7b048d0f6de486fc92e7c13eacee3e5eb292e
raviteja-ganta <ravitejareddy.ganta@gmail.com>
2021-01-08 04:43:29 -0600

    dfsdfd

--- app_v2.py
+++ app_v2.py
@@ -69,7 +69,7 @@ st.markdown(
     """,
     unsafe_allow_html=True)
 	
-
+@st.cache
 def download_file_from_google_drive(id, destination):
 	URL = 'https://docs.google.com/uc?export=download'
 
@@ -85,14 +85,14 @@ def download_file_from_google_drive(id, destination):
 	save_response_content(response,destination)
 
 
-	
+@st.cache	
 def get_confirm_token(response):
     for key, value in response.cookies.items():
         if key.startswith('download_warning'):
 			return value
 	return None
 	
-	
+@st.cache	
 def save_response_content(response, destination):
 	CHUNK_SIZE = 32768
 
