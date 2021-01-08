8a11bb3cae5faf4bb31dd3477116a8a3b0ba1d89
raviteja-ganta <ravitejareddy.ganta@gmail.com>
2021-01-08 04:26:21 -0600

    modi

--- app_v2.py
+++ app_v2.py
@@ -70,9 +70,9 @@ st.markdown(
     unsafe_allow_html=True)
 	
 def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
+	CHUNK_SIZE = 32768
 
-    with open(destination, "wb") as f:
+	with open(destination, "wb") as f:
 		for chunk in response.iter_content(CHUNK_SIZE):
 			if chunk: # filter out keep-alive new chunks
 				f.write(chunk)
