d150e65f94b8eb767978b7bffcc46315f3775a10
raviteja-ganta <ravitejareddy.ganta@gmail.com>
2021-01-08 04:38:25 -0600

    modify

--- app_v2.py
+++ app_v2.py
@@ -71,9 +71,9 @@ st.markdown(
 	
 
 def download_file_from_google_drive(id, destination):
-	URL = "https://docs.google.com/uc?export=download"
+	URL = 'https://docs.google.com/uc?export=download'
 
-    session = requests.Session()
+	session = requests.Session()
 
 	response = session.get(URL, params = {'id':id}, stream = True)
 	token = get_confirm_token(response)
